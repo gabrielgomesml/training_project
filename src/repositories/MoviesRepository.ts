c9a9e8cd62b8de34eacc54306cd8df646bbad6b9
gabrielgomesml <xgabrielgomesmelo@gmail.com>
2022-02-16 13:35:13 -0300

    Feature(Movies-crud): create movie controller, repository and route files

---
+++ src/repositories/MoviesRepository.ts
@@ -0,0 +1,76 @@
+import { Repository, EntityRepository } from 'typeorm';
+import Movies from '@models/Movies';
+import { CreateMovieDTO, ResultMovieDTO } from '../dtos/movies';
+
+@EntityRepository(Movies)
+export default class MoviesRespository extends Repository<Movies> {
+    async createMovie(
+        movie: CreateMovieDTO,
+    ): Promise<ResultMovieDTO | boolean> {
+        try {
+            const result = this.create(movie);
+
+            await this.save(result);
+
+            return result;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async listMovies(): Promise<ResultMovieDTO[] | boolean> {
+        try {
+            const result = await this.find();
+
+            return result;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async findMovieById(id: string): Promise<ResultMovieDTO | null | boolean> {
+        try {
+            const result = await this.findOne(id);
+
+            if (!result) {
+                return null;
+            }
+
+            return result;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async updateMovieById(
+        id: string,
+        movie: CreateMovieDTO,
+    ): Promise<ResultMovieDTO | null | boolean> {
+        try {
+            await this.update(id, movie);
+
+            const updatedMovie = await this.findOne(id);
+
+            if (!updatedMovie) {
+                return null;
+            }
+
+            return updatedMovie;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async deleteMovieById(id: string) {
+        try {
+            const result = await this.delete(id);
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRoute from './usersRoute';
 import genresRoute from './genresRoute';
+import moviesRoute from './moviesRoute';
 
 const router = Router();
 
 router.use(usersRoute);
 router.use(genresRoute);
+router.use(moviesRoute);
 
 export default router;
